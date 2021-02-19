f430f42cedb0b0ada178118ff47576c05bfb21be
jay-m-simformsolutions <jay.m@simformsolutions.com>
2021-02-19 10:46:12 +0530

    case apis added

--- src/routes/pharma.js
+++ src/routes/pharma.js
@@ -4,25 +4,10 @@ const express = require('express'),
       { getToken, verifyToken } = require('../Controllers/auth'),
       { pharmaAccess } = require('../Controllers/permission'),
       pharmaRoute = express.Router()
-
-pharmaRoute.get('/', (req,res) => {client
-    .query("select * from pharmacists")
-    .then(resp => {
-        res.status(200).send(resp.rows)
-    })
-    .catch(e => {
-        res.status(503).send("Connection Error")
-        console.error(e.stack)
-    })
-})
-
-pharmaRoute.get('/id', verifyToken, pharmaAccess, (req,res) => {
-    res.status(200).send(`${req.body.userId}`)
-})
-
+      
 pharmaRoute.post('/signin', async (req,res) => {
     try {
-        const resp = await client.query(`select * from pharmacists where "pharmaID"=${req.body.pharmaID}`)
+        const resp = await client.query('SELECT * FROM pharmacists WHERE "pharmaID"=$1',[req.body.pharmaID])
 
         const result = await comparePass(req.body.pharmaPass,resp.rows[0].pharmaPass)
         if( result ) {
@@ -39,7 +24,7 @@ pharmaRoute.post('/signin', async (req,res) => {
 
 pharmaRoute.post('/signup', async (req,res) => {
     try {
-        const text = ['INSERT INTO pharmacists VALUES($1, $2, $3) RETURNING *', `SELECT * FROM pharmacists where "pharmaID"=${req.body.pharmaID}`],
+        const text = ['INSERT INTO pharmacists VALUES($1, $2, $3) RETURNING *', 'SELECT * FROM pharmacists where "pharmaID"=$1'],
               values = [req.body.pharmaID, req.body.pharmaPass, req.body.pharmaName]
         
         const user = await client.query(text[1])
@@ -54,6 +39,44 @@ pharmaRoute.post('/signup', async (req,res) => {
         }
     } catch(e) {
         res.status(500).json({ message:'Something went wrong! Please try again', errors: e.stack })
+    }
+})
+
+pharmaRoute.get('/', (req,res) => {
+    client
+    .query("SELECT * FROM pharmacists")
+    .then(resp => {
+        res.status(200).send(resp.rows)
+    })
+    .catch(e => {
+        res.status(503).send("Connection Error")
+        console.error(e.stack)
+    })
+})
+
+pharmaRoute.use(verifyToken)
+pharmaRoute.use(pharmaAccess)
+
+pharmaRoute.get('/:pharmaID', async (req,res) => {
+    try{
+        const resp = await client.query('SELECT * FROM pharmacists WHERE "pharmaID"=$1',[req.params.pharmaID])
+        delete resp.rows[0].pharmaPass
+        res.status(200).json({pharmacists: resp.rows[0]})
+    } catch (e) {
+        res.status(404).json({message: "User Not Found!", errors: e.stack})
+    }
+})
+
+pharmaRoute.get('/:pharmaID/:patientID', async (req, res) => {
+    if(req.params.pharmaID === req.user._id) {
+        try {
+            const resp = await client.query('SELECT cases."caseID",cases."prescription" FROM cases WHERE "patientID"=$1',[req.params.patientID])
+            res.status(200).json({ prescriptions: resp.rows })
+        } catch (e) {
+            res.status(403).json({ message: 'Something went wrong! Please try again later', errors: e.stack })
+        }
+    } else {
+        res.status(403).json({ message: 'You are not authorized'})
     }
 })
 
